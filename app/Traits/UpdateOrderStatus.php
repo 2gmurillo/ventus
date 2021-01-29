58091187df72de00f358dc300ece23a47c0dc84d
2gmurillo <2gmurillo@gmail.com>
2021-01-29 06:53:25 -0500

    refactor: app/Trait/UpdateOrderStatus
    - single responsability

--- app/Traits/UpdateOrderStatus.php
+++ app/Traits/UpdateOrderStatus.php
@@ -13,20 +13,30 @@ trait UpdateOrderStatus
     public function updateOrderStatus(Order $order): void
     {
         $previousOrderStatus = $order->status;
-        if ($previousOrderStatus === Order::IN_PROCESS || ($previousOrderStatus === Order::PENDING)) {
+        if (($previousOrderStatus === Order::IN_PROCESS) || ($previousOrderStatus === Order::PENDING)) {
             $paymentGateway = PaymentGatewayFactory::create($order->payment_gateway_id);
             $currentOrderStatus = $paymentGateway->status($order->reference);
-            if ($previousOrderStatus !== $currentOrderStatus) {
-                $order->status = $currentOrderStatus;
-                $order->save();
-                Order::flushCache();
-            }
-            if ($currentOrderStatus === Order::REJECTED) {
-                foreach ($order->products as $product) {
-                    $quantity = $product->pivot->quantity;
-                    $product->increment('stock', $quantity);
-                    Product::flushCache();
-                }
+            $this->compareStatuses($order, $previousOrderStatus, $currentOrderStatus);
+            $this->returnProductsToStock($order, $currentOrderStatus);
+        }
+    }
+
+    public function compareStatuses(Order $order, string $previousOrderStatus, string $currentOrderStatus): void
+    {
+        if ($previousOrderStatus !== $currentOrderStatus) {
+            $order->status = $currentOrderStatus;
+            $order->save();
+            Order::flushCache();
+        }
+    }
+
+    public function returnProductsToStock(Order $order, string $currentOrderStatus): void
+    {
+        if ($currentOrderStatus === Order::REJECTED) {
+            foreach ($order->products as $product) {
+                $quantity = $product->pivot->quantity;
+                $product->increment('stock', $quantity);
+                Product::flushCache();
             }
         }
     }
